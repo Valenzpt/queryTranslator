881a93cf64338442095a4f255b1fa3c0461e576a
Valentina Zapata <valentinazg86@gmail.com>
2024-12-03 07:47:13 -0500

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as cookies from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    maxAge: 24 * 60 * 60 * 5,
+  };
+  app.enableCors(corsOptions);
+  app.use(cookies());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
-    })
+      forbidNonWhitelisted: true,
+    }),
   );
   await app.listen(process.env.PORT ?? 3000);
 }
